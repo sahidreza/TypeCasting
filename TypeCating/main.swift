72f5ee2f27e8ae511e4f5f5a7beaa6af242a6b1a
sahidcse15@gmail.com <sahidcse15@gmail.com>
2022-12-28 13:38:41 +0530

    Intial commot

--- TypeCating/main.swift
+++ TypeCating/main.swift
@@ -5,7 +5,64 @@
 //  Created by Sahid Reza on 28/12/22.
 //
 
-import Foundation
+//import Foundation
+//
+//class Animal {
+//
+//    var name:String
+//
+//    init(name: String) {
+//        self.name = name
+//    }
+//
+//
+//}
+//
+//class Human:Animal {
+//
+//    func code(){
+//        print("typing Awy")
+//    }
+//
+//}
+//
+//class Fish :Animal {
+//    func breathUnderwater(){
+//        print("Brithing under water")
+//    }
+//}
+//
+//
+//let abu = Human(name: "Abu")
+//let jack = Human(name: "jack")
+//let nemo = Fish(name: "Nemo Foush")
+//
+//let neughbours = [abu,jack,nemo]
+//let num = 12
+//
+//let neigbhours:[Any] = [abu,jack,nemo,num]
 
-print("Hello, World!")
+// Any(all objects), Anyoject(only for class Instance),NSObjct(apple foundation nsobject)
+
+
+//func findNemo(from animals:[Animal]){
+//
+//    for animal in animals {
+//
+//        if animal is Fish {
+//            print(animal.name)
+//
+//            let fish = animal as! Fish
+//            let animalFish = fish as Animal
+//
+//        }
+//
+//    }
+//}
+//
+//
+//findNemo(from: neughbours)
+//
+//let fish = neughbours[2] as? Fish
+//fish?.breathUnderwater()
 
